1c5cc9cc034cbfe30f6e1d790514618577c673eb
Cole Arendt <cole.arendt@outlook.com>
2018-08-23 00:08:08 +0000

    add example dynamic classroom app

---
+++ dynamic-classroom/app.R
@@ -0,0 +1,85 @@
+library(shiny)
+library(magrittr)
+#library(shinycssloaders)
+
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+    uiOutput("page_0") %>% withSpinner(),
+    uiOutput("page_1"),
+    uiOutput("page_2")
+)
+
+server <- function(input, output) {
+    
+    state <- reactiveVal(0, label = "state")
+    
+    # state = 0 : prompt for password
+    output$page_0 <- renderUI({
+        req(state() == 0);
+        
+        div(
+            h2("Hello")
+            , p("What is going on?")
+            , textInput("text_0", "Input Classroom Password:")
+            , actionButton("submit_0", "Submit")
+        )
+    })
+    
+    # check that password matches a classroom
+    observeEvent(input$submit_0, {
+        state(1);
+    })
+    
+    # state = 1 : prompt for name and email
+    output$page_1 <- renderUI({
+        req(state() == 1);
+        
+        div(
+            h2("Welcome to the classroom!"),
+            p("Please enter your name and email address"),
+            textInput("name_1", "Name: "),
+            textInput("email_1", "Email: "),
+            actionButton("submit_1", "Submit")
+        )
+        
+    })
+    
+    observeEvent(input$submit_1, {
+        showModal(
+            modalDialog(
+                div(
+                    p("This application uses cookies to ensure that you have a good user experience. Do you give your consent to do so?")
+                )
+                , title = "Your Information"
+                , footer = div(actionButton("no_modal_1", "No"), actionButton("yes_modal_1", "Yes"))
+                )
+            )
+        
+    })
+        
+    
+    observeEvent(input$yes_modal_1, {
+        removeModal()
+        state(2)
+    })
+    
+    observeEvent(input$no_modal_1, {
+        removeModal()
+        state(0)
+    })
+    
+    output$page_2 <- renderUI({
+        req(state() == 2);
+        
+        div(
+            h3("Your classroom information is below")
+        )
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
