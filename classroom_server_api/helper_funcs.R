d57a71df3fbc7107dfd5f6ab006ae3da0361d1bd
Alex Gold <alexkgold@gmail.com>
2019-10-18 15:51:39 -0400

    Add classroom server API

--- classroom_server_api/helper_funcs.R
+++ classroom_server_api/helper_funcs.R
@@ -1,17 +1,47 @@
 # Various Helpers for Classroom Management API
+library(plumber)
+library(dplyr)
+library(DBI)
+library(magrittr)
 
-# Misc ------
+# Table Access ------
+
+tables <- function() c("classroom", "event", "instance", "student", "claim")
+
+table_valid <- function(which) {
+  which %in% tables()
+}
+
 # get table name
 class_table <- function(which) {
-  stopifnot(which %in% c("claim", "classroom", "event", "instance", "student"))
+  stopifnot(table_valid(which))
   
   paste0(prefix, which)
 }
 
-# get tables
-get <- function(table) {
-  dbReadTable(con, class_table(table))
+# get complete  tables
+get <- function(which) {
+  stopifnot(table_valid(which))
+  dbReadTable(con, class_table(which))
 }
+
+# Error handlers ----
+
+# Try, if err Update response status and return
+check_err <- function(res, ...){
+  tryCatch(..., 
+           error = function(e) {
+             res$status <- 400
+             handle_err(e)
+           })
+}
+
+# format error for return in res body
+handle_err <- function(e) {
+  list(error = jsonlite::unbox(e$message))
+}
+
+# Protect input -----
 
 # cast empty to null
 protect_empty <- function(input, out_obj = "null"){
@@ -23,20 +53,138 @@ protect_empty <- function(input, out_obj = "null"){
   }
 }
 
+# Make character strings safe
 safe_chr <- function(x) {
   x %>% glue::single_quote() %>% protect_empty()
 }
 
-get_classroom_status <- function(class_id) {
-  class_id <- as.numeric(class_id)
+# Check URL well-formed
+check_url <- function(x) {
+  if(!stringr::str_detect(x, "^http(s)?:\\/\\/.+.com")) {
+    stop("URL malformed.")
+  }
+  NULL
+}
+
+# Identify and get objects ----
+
+# Turn object IDs into character string for SQL
+make_id_str <- function(...) {
+  id_var <- names(list(...))
+  id <- c(...) %>% as.numeric()
+  
+  if(any(is.na(id))) stop("Only numeric IDs are valid.")
+  
+  paste(glue::glue("{id_var} = {id}"), collapse = " AND ")
+}
+
+# check that particular object exists
+exists <- function(what, ...) {
+  id_str <- make_id_str(...)
+  query <- glue::glue(
+    "SELECT COUNT(*)
+      FROM {class_table(what)}
+      WHERE {id_str};"
+  )
+  
+  n <- dbGetQuery(con, query)
+  
+  if (n == 0) stop("Invalid IDs, no objects found.")
+  if (n > 1) stop("Multiple objects with that ID set.")
+  
+  NULL
+}
+
+# create object in table
+create_object <- function(which, ...) {
+  fields <- names(list(...)) %>% paste(collapse = ", ")
+  values <- c(...) %>% safe_chr() %>% paste(collapse = ", ")
   
   query <- glue::glue(
-    "SELECT 
-      status 
-    FROM {class_table('classroom')} 
-    WHERE classroomid = {class_id};"
+    "INSERT INTO {class_table(which)}
+            ({fields})
+            VALUES ({values})
+            RETURNING *
+            ;"
   )
+  
+  print(query)
+  
   dbGetQuery(con, query)
+}
+
+# Identify and get attributes ------
+
+# Check that attribute is valid for type of object
+attr_valid <- function(which, attr, editable_only = FALSE) {
+  if(!attr %in% attributes(which, editable_only)) {
+    stop(glue::glue("Valid attributes are {attributes(which, editable_only)}, not {attr}."))
+  }
+  NULL
+}
+
+# Check that calling attribute of particular object is  valid
+attr_exists <-function(which , attr, ..., editable_only = FALSE) {
+  exists(which, ...)
+  attr_valid(which, attr, editable_only)
+}
+
+# Get attribute from a particular object; ... should be named ids
+get_attr <- function(which, attr, ...) {
+  id_str <- make_id_str(...)
+  query <- glue::glue(
+    "SELECT {attr}
+      FROM {class_table(which)}
+      WHERE {id_str};"
+  )
+  
+  dbGetQuery(con, query)
+}
+
+# Update attribute for particular object; ... should be named ids
+set_attr <- function(which, attr, value, ...) {
+  attr_exists(which, attr, editable_only = TRUE, ...)
+  value <- safe_chr(value)
+  
+  query <- glue::glue(
+    "UPDATE {class_table(which)}
+      SET {attr} = {value}
+      WHERE {make_id_str(...)}
+    RETURNING *;"
+  )
+  
+  dbGetQuery(con, query)
+}
+
+
+# gettable/editable attributes for each object type
+attributes <- function(which, editable_only = FALSE) {
+  edit_attrs <- list(
+    classroom = c("name", "description", "password", "status"), 
+    instance = c("identifier", "url", "username", "password"), 
+    student = c("name", "email", "consent", "cookie", "other"), 
+    event = c("event", "session", "cookie", "other")
+  )
+  
+  static_attrs <- list(
+    classroom = c("id"), 
+    instance = c("id", "classroomid"), 
+    student = c("id", "classroomid"), 
+    event = c("id", "classroomid", "instanceid", "studentid")
+  )
+  
+  attrs <- do.call(switch, c(EXPR = which, edit_attrs))
+  
+  if (!editable_only) attrs <- c(attrs, 
+                                 do.call(switch, c(EXPR = which, static_attrs)))
+  
+  attrs
+}
+
+# Specific helpers -----
+
+is_claimed <- function(instance_id) {
+  instance_id %in% get("claim")$instanceid
 }
 
 archive_class <- function(class_id) {
@@ -51,3 +199,4 @@ archive_class <- function(class_id) {
   
   dbGetQuery(con, query)
 }
+
--- classroom_server_api/plumber.R
+++ classroom_server_api/plumber.R
@@ -1,65 +1,93 @@
-library(plumber)
-library(dplyr)
-library(DBI)
-library(magrittr)
+source(here::here("classroom_server_api/helper_funcs.R"))
 
+# TODO: disallow interacting with archived classroom? (Or do on app side?)
+
+# Do some config/connect to DB
 cfg <- config::get("database", file = "config.yml")
-con <- do.call(pool::dbPool, cfg)
-prefix <- "v1_"
+con <<- do.call(pool::dbPool, cfg)
+prefix <<- "v1_"
 schema <- "classroom"
-dbExecute(con, glue("SET search_path={schema};"))
+dbExecute(con, glue::glue("SET search_path={schema};"))
+
+claim <- tbl(con, class_table("claim"))
+class <- tbl(con, class_table("classroom"))
+student <- tbl(con, class_table("student"))
+instance <- tbl(con, class_table("instance"))
 
 #* @apiTitle Classroom Server API
 
 # Classrooms ------
 
 #* Get list of classrooms
-#* @get /classrooms
-function(req, res, ...) {
+#* @get /class
+function() {
   get("classroom")
 }
 
-
-
 #* Create a classroom
-#* @post /create_class
-function(name, password, description) {
-  db_insert_into(con, class_table("classroom"), 
-                 tibble::tibble(name = name, 
-                                password = password, 
-                                description = description))
-}
-
-#* Change Classroom Status
-#* @param class_id classroomid from classroom table
-#* @param status either ACTIVE or ARCHIVE
-#* @param ... 
-#* @post /classroom_status
-function(req, res, class_id, status, ...) {
-  # Safe inputs
-  class_id <- as.numeric(class_id)
-  stopifnot(status %in% c("ACTIVE", "ARCHIVE"))
+#* @param name name of classroom
+#* @param password classroom password
+#* @param description classroom description, defaults to NULL
+#* @post /class
+function(res, name, password, description = '') {
+  classrooms <- get("classroom")
   
-  # Can't reactivate archived class
-  curr_status <- get_classroom_status(class_id)
-  if (status == "ACTIVE" && curr_status == "ARCHIVE") {
-    # TODO: figure out how to return informative error
-    msg <- "Cannot reactivate archived class."
+  if (name %in% classrooms$name || password %in% classrooms$password) {
+    msg <- "Classroom names and passwords must be unique. Yours is not."
     res$status <- 400
     return(list(error = jsonlite::unbox(msg)))
   }
   
+  create_object("classroom", 
+                name = name, 
+                password = password, 
+                description = description)
+}
+
+#* Get a classroom attribute
+#* @param class_id classroomid from classroom table
+#* @param attr attribute
+#* @get /class_attr
+function(res, class_id, attr) {
+  class_id <- as.numeric(class_id)
+  print(class_id)
+  print(attr)
+  err <- check_err(res, 
+                   attr_exists("classroom", attr, editable_only = TRUE, 
+                               classroomid = class_id))
+  if (!is.null(err)) return(err)
+  
+  get_attr("classroom", attr, classroomid = class_id)
+}
+
+#* Update classroom attributes
+#* @param class_id classroomid from classroom table
+#* @param attr attribute
+#* @param value new value
+#* @post /class_attr
+function(res, class_id, attr, value) {
+  # Check inputs valid
+  class_id <- as.numeric(class_id)
+  err <- tryCatch({
+    attr_exists("classroom", attr, classroomid = class_id, editable_only = TRUE)
+    # Statuses have special requirements
+    if (attr == "status"){
+      curr_status <- get_attr("classroom", "status",classroomid = class_id)
+      if (!value %in%  c("ACTIVE", "ARCHIVE")) {
+        stop("Only ACTIVE and ARCHIVE are valid statuses.")
+      } else if (curr_status == "ARCHIVE" && value == "ACTIVE") {
+        stop("Cannot reactivate ARCHIVEd classrooms.")
+      }
+    }
+  }, error = function(e) {
+    res$status <- 400
+    handle_err(e)
+  })
+  if (length(err) > 0) return(err)
   # suffix name, password, and description
-  if (status == "ARCHIVE") archive_class(class_id)
+  if (attr == "status" && value == "ARCHIVE") archive_class(class_id)
   
-  query <- glue::glue(
-    "UPDATE {class_table('classroom')}
-      SET status = '{status}'
-      WHERE classroomid = {class_id}
-    RETURNING *;"
-  )
-  
-  dbGetQuery(con, query)
+  set_attr("classroom", attr, value, classroomid = class_id)
 }
 
 # Students -----
@@ -75,31 +103,42 @@ function(class_id = NULL) {
   df
 }
 
-#* Add a student
+#* Create a student
 #* @param class_id classroomid from class table
 #* @param name full student name
 #* @param email student email
 #* @param other other details, defaults to NULL
 #* @post /student
-function(req, res, class_id, name, email, other = NULL) {
+function(req, res, class_id, name, email, other = '') {
   class_id <- as.numeric(class_id)
-  name <- safe_chr(name)
-  email <- safe_chr(email)
-  other <- safe_chr(other)
   
-  if (!class_id %in% get("classroom")$classroomid) {
-    msg <- "Invalid class id."
-    res$status <- 400
-    return(list(error = jsonlite::unbox(msg)))
-  }
+  err <- check_err(res, exists("classroom", classroomid = class_id))
+  if (!is.null(err)) return(err)
   
-  query <- glue::glue(
-    "INSERT INTO {class_table('student')}
-      (classroomid, name, email, other)
-      VALUES ({class_id}, {name}, {email}, {other})
-      RETURNING *;"
-  )
-  dbGetQuery(con, query)
+  create_object("student", 
+                classroomid = class_id, 
+                name = name, 
+                email = email, 
+                other = other)
+}
+
+#* Get a student attribute
+#* @param class_id classroom id
+#* @param student_id student id
+#* @param attr attribute
+#* @get /student_attr
+function(res, class_id, student_id, attr) {
+  class_id <- as.numeric(class_id)
+  student_id <- as.numeric(student_id)
+  
+  err <- check_err(res, 
+                   attr_exists("student", attr, 
+                               studentid = student_id, 
+                               classroomid = class_id))
+  if (!is.null(err)) return(err)
+  
+  get_attr("student", attr, 
+           studentid = student_id, classroomid = class_id)
 }
 
 #* Update a student attribute
@@ -107,35 +146,31 @@ function(req, res, class_id, name, email, other = NULL) {
 #* @param student_id student id
 #* @param attribute attribute, one of name, email, consent, cookie, other
 #* @param value value to update
-#* @post /update_student
-function(class_id, student_id, attribute, value) {
+#* @post /student_attr
+function(res, class_id, student_id, attr, value) {
   class_id <- as.numeric(class_id)
   student_id <- as.numeric(student_id)
-  value <- safe_chr(value)
   
-  stopifnot(attribute %in% c("name", "email", "consent", "cookie", "other"))
+  err <- check_err(res, 
+                   attr_exists("student", 
+                               attr, 
+                               studentid = student_id, 
+                               classroomid = class_id))
+  if (!is.null(err)) return(err)
   
-  query <- glue::glue(
-    "UPDATE {class_table('student')}
-      SET {attribute} = {value}
-      WHERE classroomid = {class_id} AND studentid = {student_id}
-    RETURNING *;"
-  )
-  dbGetQuery(con, query)
+  set_attr("student", attr, value, 
+           classroomid = class_id, studentid = student_id)
 }
 
 # Instances ------
 
 #* Get Instances
 #* @param class_id id of classroom
-#* @param unclaimed only unclaimed instances? defaults to FALSE
 #* @get /instance
-function(class_id = NULL, unclaimed = FALSE) {
+function(class_id = NULL) {
   df <- get("instance")
   
   if (!is.null(class_id)) df <- dplyr::filter(df, classroomid == class_id)
-  
-  # TODO: Make unclaimed work
   
   df
 }
@@ -147,30 +182,204 @@ function(class_id = NULL, unclaimed = FALSE) {
 #* @param username instance username
 #* @param password instance password
 #* @post /instance
-function(req, res, class_id, identifier, url, username, password) {
+function(res, class_id, identifier, url, username, password) {
   class_id <- as.numeric(class_id)
-  identifier %<>% safe_chr()
-  url %<>% safe_chr()
-  username %<>% safe_chr()
-  password %<>% safe_chr()
   
-  if (!class_id %in% get("classroom")$classroomid) {
-    msg <- "Invalid class id."
-    res$status <- 400
-    return(list(error = jsonlite::unbox(msg)))
-  }
+  err <- check_err(res, 
+                   attr_exists("classroom", "id", classroomid = class_id),
+                   check_url(url))
+  if (!is.null(err)) return(err)
   
-  query <- glue::glue(
-    "INSERT INTO {class_table('instance')}
-      (classroomid, identifier, url, username, password)
-      VALUES ({class_id}, {identifier}, {url}, {username}, {password})
-      RETURNING *;"
-  )
-    
-    dbGetQuery(con, query)
+  create_object("instance", 
+                classroomid = class_id,
+                identifier = identifier, 
+                url = url, 
+                username = username, 
+                password = password)
 }
 
-# TODO: Allow calling with list of instances?
-# TODO: Allow update to instance attributes
-# TODO: Allow claiming instances
-# TODO: Add event logging.
+#* Get Instance attributes
+#* @param class_id classroom id
+#* @param instance_id instance id
+#* @param attr attribute
+#* @get /instance_attr
+function(res, class_id, instance_id, attr) {
+  err <- check_err(res, 
+                   attr_exists("instance", attr, 
+                               classroomid = class_id, 
+                               instanceid = instance_id))
+  if (!is.null(err)) return(err)
+  
+  get_attr("instance", attr, 
+           classroomid = class_id, instanceid = instance_id)
+}
+
+#* Update instance attributes
+#* @param class_id classroom id
+#* @param instance_id instance id
+#* @param attr attribute
+#* @param value value
+#* @post /instance_attr
+function(res, class_id, instance_id, attr, value) {
+  class_id <- as.numeric(class_id)
+  student_id <- as.numeric(instance_id)
+  
+  err <- tryCatch({
+    attr_exists("instance", 
+                attr, 
+                instanceid = instance_id, 
+                classroomid = class_id)
+    if(attr == "url") check_url(value)
+  }, error = function(e) {
+    res$status <- 400
+    handle_err(e)
+  })
+  if (!is.null(err)) return(err)
+  
+  
+  set_attr("instance", attr, value, 
+           classroomid = class_id, instanceid = instance_id)
+}
+
+
+#* Allow student to claim an instance
+#* @param class_id classroom
+#* @param student_id student
+#* @param instance_id instance, defaults to NULL (find an instance)
+#* @post /claim_instance
+function(res, class_id, student_id, instance_id = NULL) {
+  class_id <- as.numeric(class_id)
+  student_id <- as.numeric(student_id)
+  if(!is.null(instance_id)) instance_id <- as.numeric(instance_id)
+  
+  err <- tryCatch({
+    # check if instance claimed, if supplied
+    if(!is.null(instance_id) && is_claimed(instance_id)) {
+      stop("That instance is claimed")
+    }
+    exists("classroom", classroomid = class_id)
+    exists("student", classroomid = class_id, studentid = student_id)
+    # Check if class, student, and instance exist
+    if (!is.null(instance_id)) exists("instance", 
+                                      classroomid = class_id, 
+                                      instanceid = instance_id)
+  }, error = function(e){
+    res$status <- 400
+    return(handle_err(e))
+  })
+  if (!is.null(err)) return(err)
+  
+  # If instance not specified
+  if (is.null(instance_id)) {
+    tbl(con, class_table('instance')) %>%
+      anti_join(tbl(con, class_table('claim')), 
+                by = c("instanceid")) %>%
+      head(1) %>% 
+      pull(instanceid) %>%
+      create_object("claim", 
+                    instanceid = ., 
+                    classroomid = class_id, 
+                    studentid = student_id)
+  } else {
+    # Instance specified
+    create_object("claim", 
+                  classroomid = class_id, 
+                  instanceid = instance_id, 
+                  studentid = student_id)
+  }
+}
+
+#* Get students and instance details for a classroom
+#* @param class_id class id
+#* @get /student_instances
+function(class_id) {
+  tbl(con, class_table('claim')) %>%
+    select(classroomid,
+           studentid,
+           instanceid) %>%
+    dplyr::filter(classroomid == class_id) %>%
+    left_join(
+      select(
+        tbl(con, class_table('classroom')),
+        classroomid,
+        class_description = description,
+        class_name = name)) %>%
+    left_join(
+      select(tbl(con, class_table('instance')),
+             classroomid,
+             instanceid,
+             instance_identifier = identifier,
+             instance_url = url,
+             instance_user = username,
+             instance_pass = password)) %>%
+    left_join(
+      select(tbl(con, class_table('student')),
+             classroomid,
+             studentid,
+             student_name = name,
+             student_email = email)) %>%
+    select(-ends_with("id")) %>%
+  collect()
+}
+
+# Events -----
+
+#* Get Events
+#* @get /event
+function() {
+  get("event")
+}
+
+#* Add event
+#* @param class_id classroom id
+#* @param instance_id instance id
+#* @param student_id student id
+#* @param event event
+#* @param session session
+#* @param cookie cookie
+#* @param other other
+#* @post /event
+function(res, event, session, 
+         class_id = NULL, instance_id = NULL, student_id = NULL,
+         cookie = NULL, other = NULL) {
+  
+  tryCatch({
+    if((!is.null(class_id) && !exists("classroom", classroomid = class_id)) ||
+       (!is.null(instance_id) && !exists("instance", instanceid = instance_id)) ||
+       (!is.null(student_id) && !exists("student", studentid = student_id))) {
+      stop("IDs do not exist.")
+    }
+  }, 
+  error = function(e) {
+    res$status <- 400
+    return(handle_err(e))
+  })
+  
+  args <- list(
+    which = "event", 
+    event = event, 
+    session = session
+  )
+  
+  if(!is.null(class_id)) args$classroomid <- class_id
+  if(!is.null(student_id)) args$studentid <- student_id
+  if(!is.null(instance_id)) args$instanceid <- instance_id
+  if(!is.null(cookie)) args$cookie <- cookie
+  if(!is.null(other)) args$other <- other
+  
+  do.call(create_object, args)
+}
+
+#* Get Event attributes
+#* @param event_id event id
+#* @param attr attribute
+#* @get /event_attr
+function(res, event_id, attr) {
+  err <- check_err(res, 
+                   attr_exists("event", attr, eventid = event_id))
+  if (!is.null(err)) return(err)
+  
+  get_attr("event", attr, eventid = event_id)
+}
+
+# Intentionally excluding /event_attr post -- shouldn't be able to update once created.
